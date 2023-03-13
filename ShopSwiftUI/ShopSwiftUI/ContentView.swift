b1c6eff575e73b14eb0d8793d8329c233dd8dbed
samanteras <samanter@mail.ru>
2023-03-14 01:28:16 +0300

    Sign in page

--- ShopSwiftUI/ShopSwiftUI/ContentView.swift
+++ ShopSwiftUI/ShopSwiftUI/ContentView.swift
@@ -9,57 +9,79 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    //  @State private var signIn = ""
+        @State private var firstName = ""
+        @State private var lastName = ""
+        @State private var email = ""
+        @State private var wrongEmail = 0
+        @State private var showingScreen = false
+    
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            ZStack {
+                Color(.systemGray5).edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("Sign in")
+                        .padding(.bottom, 80)
+                        .bold()
+                        .font(.largeTitle)
+                    TextField("First name", text: $firstName)
+                        .padding()
+                        .frame(width: 300, height: 40)
+                        .multilineTextAlignment(.center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(25)
+                        .padding(.bottom, 30)
+                    TextField("Last name", text: $firstName)
+                        .padding()
+                        .frame(width: 300, height: 40)
+                        .multilineTextAlignment(.center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(25)
+                        .padding(.bottom, 30)
+                    TextField("Email", text: $firstName)
+                        .padding()
+                        .frame(width: 300, height: 40)
+                        .multilineTextAlignment(.center)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(25)
+                        .border(.red, width: CGFloat(wrongEmail))
+                        .padding(.bottom, 30)
+                    Button("Sign in") {
+                        autheticateUser(email: email)
                     }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 70)
+                    //  .padding()
+                    .background(Color.indigo)
+                    .cornerRadius(25)
+                    
                 }
-                .onDelete(perform: deleteItems)
+                NavigationLink(
+                    destination: Text("You are logged in @\(firstName)"),
+                    isActive: $showingScreen,
+                    label: {
+                    EmptyView()
+                })
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
+        .navigationBarHidden(true)
     }
-
-    private func addItem() {
-      
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-   
+    
+    func autheticateUser(email: String) {
+        if email.hasPrefix("@mail.ru") {
+            wrongEmail = 0
+            showingScreen = true
+        } else {
+            wrongEmail = 2
+        }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
